d1d5c363c063858a1fd1c3a1fee9d79acd8dcbee
Maria <cparauso09@gmail.com>
2023-06-12 16:54:44 -0300

    carrousel de series

---
+++ src/components/datas/series.js
@@ -0,0 +1,65 @@
+const DATA = 
+[
+    {
+        id:"01",
+        nome:"Breaking Bad",
+        nota:'9.0',
+        imagem:'breakingbad.jpg'
+    },
+    {
+        id:"02",
+        nome:"Euphoria",
+        nota:'10.0',
+        imagem:'euphoria.jpg'
+    },
+    {
+        id:"03",
+        nome:"Skins",
+        nota:'9.9',
+        imagem:'skins.jpg'
+    },
+    {
+        id:"04",
+        nome:"You",
+        nota:'9.0',
+        imagem:'you.jpg'
+    },
+    {
+        id:"05",
+        nome:"Big Mouth",
+        nota:'9.7',
+        imagem:'bigmouth.jpg'
+    },
+    {
+        id:"06",
+        nome:"Elite",
+        nota:'8.7',
+        imagem:'elite.jpg'
+    },
+    {
+        id:"07",
+        nome:"13 Reasons Why",
+        nota:'8.4',
+        imagem:'13rw.jpg'
+    },
+    {
+        id:"08",
+        nome:"The Society",
+        nota:'9.5',
+        imagem:'thesociety.jpg'
+    },
+    {
+        id:"09",
+        nome:"Outer Banks",
+        nota:'9.7',
+        imagem:'outerbanks.jpg'
+    },
+    {
+        id:"10",
+        nome:"The Wilds",
+        nota:'8.9',
+        imagem:'thewilds.jpg'
+    }
+]
+
+export default DATA;
